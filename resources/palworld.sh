28125c19a871d4eedcaeff44d0389adabc1eb573
Spirit1998HD <deniskroeger@outlook.de>
2024-01-20 11:36:18 +0000

    copy default config if no one exists

--- resources/palworld.sh
+++ resources/palworld.sh
@@ -10,7 +10,13 @@ GAMECONFIG_DIR=$INSTALL_DIR/PalServer/Pal/Saved/Config/LinuxServer
 
 mkdir -p $GAMECONFIG_DIR
 
-touch $GAMECONFIG_DIR/PalWorldSettings.ini
+if ! [ -f $GAMECONFIG_DIR/PalWorldSettings.ini ]; then
+    cp -f $INSTALL_DIR/DefaultPalWorldSettings.ini $GAMECONFIG_DIR/PalWorldSettings.ini
+fi
+
+if ! [ -s $GAMECONFIG_DIR/PalWorldSettings.ini ]; then
+    cp -f $INSTALL_DIR/DefaultPalWorldSettings.ini $GAMECONFIG_DIR/PalWorldSettings.ini
+fi
 
 cd $INSTALL_DIR
 
