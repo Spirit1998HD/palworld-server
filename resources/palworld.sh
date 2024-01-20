e7339c6a15d9f04e3ff3db3645fbc8eb9a0d8560
Spirit1998HD <deniskroeger@outlook.de>
2024-01-20 13:52:06 +0000

    added universal env interpreter

--- resources/palworld.sh
+++ resources/palworld.sh
@@ -12,11 +12,20 @@ mkdir -p $GAMECONFIG_DIR
 
 if ! [ -f $GAMECONFIG_DIR/PalWorldSettings.ini ]; then
     cp -f $INSTALL_DIR/DefaultPalWorldSettings.ini $GAMECONFIG_DIR/PalWorldSettings.ini
+    sed -i "s/;.*//g" $GAMECONFIG_DIR/PalWorldSettings.ini > /dev/null 2>&1
+    sed -i ':a;N;$!ba;s/\n//g' $GAMECONFIG_DIR/PalWorldSettings.ini > /dev/null 2>&1
 fi
 
 if ! [ -s $GAMECONFIG_DIR/PalWorldSettings.ini ]; then
     cp -f $INSTALL_DIR/DefaultPalWorldSettings.ini $GAMECONFIG_DIR/PalWorldSettings.ini
+    sed -i "s/;.*//g" $GAMECONFIG_DIR/PalWorldSettings.ini > /dev/null 2>&1
+    sed -i ':a;N;$!ba;s/\n//g' $GAMECONFIG_DIR/PalWorldSettings.ini > /dev/null 2>&1
 fi
+
+while IFS='=' read -r -d '' name value; do
+    sed -i "s/$name=[^,()]*[)]/$name=$value)/g" $GAMECONFIG_DIR/PalWorldSettings.ini > /dev/null 2>&1
+    sed -i "s/$name=[^,()]*[,]/$name=$value,/g" $GAMECONFIG_DIR/PalWorldSettings.ini > /dev/null 2>&1
+done < <(env -0)
 
 cd $INSTALL_DIR
 
